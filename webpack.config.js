1da20965bd66547e2f9fed3fa393842de0fa5fd6
Julio Suriano <jjsuriano@gmail.com>
2020-12-14 14:02:20 -0600

    modified webpack config file and moved code from main.js to index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,21 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-// commonJS
 module.exports = {
   module: {
     rules: [
-      // Cargar los loaders, que necesitamos para que webpack haga lo que necesitamos
       {
-        test: /\.html/, // Definir lo que tengo que buscar
+        test: /\.html/,
         use: [
-          // lo que yo encontre, qué voy a hacer con ello.
           {
-            loader: "html-loader", // El nombre del loader, que va a traducir el html para que webpack lo entienda.
+            loader: "html-loader",
             options: { minimize: true },
           },
         ],
       },
       {
-        test: /\.js$/, // va a buscar archivos de js
-        exclude: /node_modules/, // significa que no va a buscar en la carpeta de no_modules
+        test: /\.js$/,
+        exclude: /node_modules/,
         use: {
           loader: "babel-loader",
           options: {
@@ -27,16 +24,12 @@ module.exports = {
         },
       },
       {
-        test: /\.(png|jpg|svg|jpeg|gif)$/, // va a buscar imágenes
-        use: ["file-loader"], // nombre del loader para las imágenes
+        test: /\.(png|jpg|svg|jpeg|gif)$/,
+        use: ["file-loader"],
       },
       {
-        test: /\.scss$/, // va a buscar archivos de sass
-        use: [
-          "style-loader", //estilos en linea de css
-          "css-loader", // procesador de los archivos de sass
-          "sass-loader", //procesador de los archivos de sass
-        ], // nombre del loader para las imágenes
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
@@ -48,7 +41,7 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: "[name].css",
-      chunkFilename: "[id].css", // esto es para manejar muchas lineas de código
+      chunkFilename: "[id].css",
     }),
   ],
 };
